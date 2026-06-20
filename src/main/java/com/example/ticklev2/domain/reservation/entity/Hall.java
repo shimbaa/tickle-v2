422422f738cc0671d1383bfb7dec0d596b4d09e6
shimbaa <shim9597@gmail.com>
2026-06-20 20:57:20 +0900

    feat: 공연장 엔티티 구현

---
+++ src/main/java/com/example/ticklev2/domain/reservation/entity/Hall.java
@@ -0,0 +1,29 @@
+package com.example.ticklev2.domain.reservation.entity;
+
+import jakarta.persistence.*;
+import java.time.Instant;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name = "hall")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Hall {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hall_id")
+    private Long id;
+
+    @Column(name = "hall_name", nullable = false, length = 50)
+    private String name;
+
+
+    @Column(name = "hall_address", nullable = false, length = 100)
+    private String address;
+
+    @Column(name = "hall_created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+}
